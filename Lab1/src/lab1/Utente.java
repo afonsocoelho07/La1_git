63f770d81be6d8339319c8b2da6342974ae80b3a
Afonso <Afonso@10.179.9.7>
2026-05-18 11:59:59 +0100

    Primeira Versão

---
+++ Lab1/src/lab1/Utente.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lab1;
+
+/**
+ * 
+ */
+public class Utente {
+	public Utente(String nome, String genero, int idade, double altura, int peso) {
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+	}
+	private String nome;
+	private String genero;
+	private int idade;
+	private double altura;
+	private int peso;
+}

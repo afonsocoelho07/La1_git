e82dc2bdcd58cf18b3a8fc62deffd4d0864c52b0
Afonso <Afonso@10.179.9.7>
2026-05-18 12:02:04 +0100

    Segundo Commit

--- Lab1/src/lab1/Utente.java
+++ Lab1/src/lab1/Utente.java
@@ -19,4 +19,35 @@ public class Utente {
 	private int idade;
 	private double altura;
 	private int peso;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
 }
